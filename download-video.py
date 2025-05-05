859d67e4b8121542fdf9558e9e41309a22bda972
Anh Le <kunquan2345@gmail.com>
2025-05-05 02:06:55 -0600

    Add multithread run and retry

--- download-video.py
+++ download-video.py
@@ -1,8 +1,14 @@
+from concurrent.futures import ThreadPoolExecutor
 import subprocess
 import fetching_m3u8
 
 def download_m3u8(page_url: str, output_path: str):
     m3u8_files = fetching_m3u8.fetching_m3u8_requests(page_url)
+    
+    while len(m3u8_files) == 0:
+        print("Retry getting " + page_url)
+        m3u8_files = fetching_m3u8.fetching_m3u8_requests(page_url)
+    
     ru_m3u8 = [f for f in m3u8_files if "miruro" not in f]
     
     for file in ru_m3u8:
@@ -21,18 +27,23 @@ def download_m3u8(page_url: str, output_path: str):
             
             
 def main():
-    base_url = "https://www.miruro.tv/watch?id=153518&ep="
-    base_output = "dungeon-meshi-"
+    # keep the ep number out
+    base_url = "https://www.miruro.tv/watch?id=918&ep="
+    base_output = "gintama-s01/gintama-"
     input_pairs = []
-    for i in range(24):
+    # assuming episodes range from 1->n, just put n in there
+    for i in range(200):
         input_pairs.append((
             base_url + str(i + 1),
             base_output + str(i + 1).zfill(2) + ".mp4"
         ))
         
-    for i_pair in input_pairs:
-        url, output = i_pair
-        download_m3u8(url, output)
+    with ThreadPoolExecutor(max_workers=4) as executor:
+        executor.map(lambda pair: download_m3u8(*pair), input_pairs)
+        
+    # for i_pair in input_pairs:
+    #     url, output = i_pair
+    #     download_m3u8(url, output)
         
         
 if __name__ == "__main__":
